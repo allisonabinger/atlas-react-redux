a412ed5bbbd982c8759cfdbab35a614824fc7e30
Allison <allisonabinger@gmail.com>
2024-09-18 14:04:25 -0500

    A - 09/18 02:04 PM - added card, img hovering functionality incomplete

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,3 +1,4 @@
+import deleteSVG from "../assets/deleteCardButton.svg"
 type CardProps = {
   title: string;
   description: string;
@@ -7,7 +8,7 @@ const Card = ({ title, description }: CardProps) => {
     return (
         <div className="card group/card m-3 flex min-h-24 w-full max-w-96 flex-col items-start rounded bg-off-white-light px-4 py-2 text-blue">
             <h5 className="my-2 flex w-full items-end justify-between text-xl font-black">{title}</h5>
-            <button className="hidden group-hover/card:block"><img src="" </button>
+            <button className="hidden group-hover/card:block">{deleteSVG}</button>
             <p className="mt-0 text-left">{description}</p>
         </div>
     )
