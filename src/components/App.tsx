f50d104f44077246f571a0566786b17c68f762a5
Allison <allisonabinger@gmail.com>
2024-09-18 13:26:23 -0500

    A - 09/18 01:26 PM - Added footer component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,14 @@
 import { Provider } from "react-redux";
 import { store } from "../store";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function App() {
   return (
     <Provider store={store}>
       <div>
         <Header />
+        <Footer />
       </div>
     </Provider>
   );
---
+++ src/components/Footer.tsx
@@ -0,0 +1,26 @@
+const Footer = () => {
+  return (
+    <div className="sticky bottom-0 left-0 flex w-screen items-center justify-center space-x-8 border-t-2 border-blue bg-off-white-light p-8">
+      <form>
+        <input
+          type="text"
+          placeholder="Add List"
+          name="title"
+          className="border-0 bg-transparent text-3xl font-semibold text-blue placeholder:text-blue placeholder:opacity-50 focus:outline-none"
+        />
+        <button
+          type="submit"
+          className="mr-3 rounded bg-teal px-6 py-4 text-xl font-semibold text-off-white-light"
+        >Save</button>
+        <button
+          type="button"
+          className="rounded bg-teal px-6 py-4 text-xl font-semibold text-off-white-light"
+        >
+          Clear Board
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Footer;
