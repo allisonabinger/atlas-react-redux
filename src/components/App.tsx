95f4363a8a36b6a9341a4dfecc6133a9930d6edb
Allison <allisonabinger@gmail.com>
2024-09-18 13:19:53 -0500

    A - 09/18 01:19 PM - Added Header and fixed app to import store correctly

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,15 @@
 import { Provider } from "react-redux";
+import { store } from "../store";
 import Header from "./Header";
 
 function App() {
-  return <Provider store={store}>
-    <div>
+  return (
+    <Provider store={store}>
+      <div>
         <Header />
-    </div>
-  </Provider>;
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,13 @@
-import atlasLogo from "../assets/logo.png";
+import atlasLogo from "../assets/logo.png"
+const Header = () => {
 
-export default function Header() {
-  <div className="header">
-    <header className="sticky top-0 border-b-2 border-blue bg-off-white-light pb-8 pt-8">
-      <img className="logo mx-auto w-56" src={atlasLogo} alt="logo" />
-    </header>
-  </div>;
+    return (
+        <div className="header">
+        <header className="sticky top-0 border-b-2 border-blue bg-off-white-light pb-8 pt-8">
+          <img className="logo mx-auto w-56" src={atlasLogo} alt="logo" />
+        </header>
+      </div>
+    )
 }
+
+export default Header;
